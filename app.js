b7db5a5f10dd3fff8b14226ce460feb7d1565777
Irakli Akhvlediani <irakliakhvlediani@hotmail.com>
2023-08-02 17:25:50 +0400

    calculator

---
+++ app.js
@@ -0,0 +1,22 @@
+
+
+const a=+prompt("enter firs:")
+const o=prompt("enter operator:")
+const b=+prompt("enter second:")
+
+switch (o)  {
+    case "+":
+        console.log (a+b);
+        break;
+        case "-":
+            console.log (a-b);
+            break;
+            case "*":
+            console.log (a*b);
+        break;
+        case "/":
+            console.log (a/b);
+            break;
+            default:
+                console.log ("wrong");
+}
